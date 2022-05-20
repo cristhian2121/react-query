9926c98ff84b3a3d9696b2230570a738a237a0d7
maestro_roshi <ccdelgadop@gmail.com>
2022-05-19 23:23:02 -0500

    Add chahceTime and refetch method in local query

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -17,7 +17,11 @@ export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
   const defaultData: Treatment[] = [];
   const { data = defaultData } = useQuery(queryKeys.treatments, getTreatments, {
-    staleTime: 5000,
+    staleTime: 600000,
+    cacheTime: 900000,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
     // onError: (error) => {
     //   const title =
     //     error instanceof Error ? error.message : 'Error connecting to server';
